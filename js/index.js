7028882b36d6e95bd1dee7c29a5fd8fecff88832
Carter Rink <carter.m.rink@gmail.com>
2019-08-19 17:36:43 -0400

    added try/catch

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
 async function getMPData() {
-    const result = await fetch('./masterpiece_data.json');
-    const data = await result.json();
-    console.log(data);
-    return data;
+    try {
+        const result = await fetch('./masterpiece_data.json');
+        const data = await result.json();
+        console.log(data);
+        return data;
+    }
+    catch(error) {
+        alert(error.message);
+    }
 }
 
-getMPData();
+getMPData().then(data => {
+
+});
