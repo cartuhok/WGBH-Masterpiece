6597344539688a5ef2d05c0128fb41a80dc6fae1
Carter Rink <carter.m.rink@gmail.com>
2019-08-20 20:37:57 -0400

    added template literal

--- js/index.js
+++ js/index.js
@@ -10,6 +10,32 @@ async function getMPData() {
     }
 }
 
-getMPData().then(data => {
+const DOMstrings = {
+    shows: document.querySelector('.shows')
+}
 
+getMPData().then(data => {
+    const markup = `<div class="show--1">
+    <figure class="img--1">
+        <img src="${data.franchise.programs[0].weekly_eepisode.image}" />
+    </figure>
+    <figcaption>
+        <h3>${data.franchise.programs[0].weekly_eepisode.date} | ${data.franchise.programs[0].weekly_eepisode.time} ET</h3>
+        <h1>${data.franchise.programs[0].title.toUpperCase()}</h1>
+        <h2>${data.franchise.programs[0].weekly_eepisode.title}</h2>
+        <p>${data.franchise.programs[0].description}</p>
+    </figcaption>
+    </div>
+    <div class="show--2">
+        <figure class="img--2">
+            <img src="${data.franchise.programs[1].weekly_eepisode.image}" />
+        </figure>
+        <figcaption>
+        <h3>${data.franchise.programs[1].weekly_eepisode.date} | ${data.franchise.programs[0].weekly_eepisode.time} ET</h3>
+        <h1>${data.franchise.programs[1].title.toUpperCase()}</h1>
+        <h2>${data.franchise.programs[1].weekly_eepisode.title}</h2>
+        <p>${data.franchise.programs[1].description}</p>
+        </figcaption>
+    </div>`
+    DOMstrings.shows.insertAdjacentHTML('beforeend', markup);
 });
